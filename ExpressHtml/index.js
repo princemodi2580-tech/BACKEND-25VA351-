cb11207147586ccf2a496a0d280e91995822da9b
25VA151 <you@example.com>
2026-09-11 12:36:44 +0530

    Commit

---
+++ ExpressHtml/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    fs.readFile('./pages/home.html','utf-8',(err,data)=>{
+        if(err){
+            res.status(500).send('Error reading file');
+            return;
+        } else{
+            res.send(data);
+        }
+    });
+});
+app.get('./pages/about',(req,res)=>{
+    fs.readFile('./pages/about.html','utf-8',(err,data)=>{
+        if(err){
+            res.status(500).send('Error reading file');
+            return;
+        } else{
+            res.send(data);
+        }
+    });
+});
+app.get('./pages/contact',(req,res)=>{
+    fs.readFile('./pages/contact.html','utf-8',(err,data)=>{
+        if(err){
+            res.status(500).send('Error reading file');
+            return;
+        } else{
+            res.send(data);
+        }
+    });
+});
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+});
